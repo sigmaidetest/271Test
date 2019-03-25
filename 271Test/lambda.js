815aa32e47b21f4bb666e77ea3da3418d3b11589
sigmaidetest <cehib@balanc3r.com>
2019-03-25 10:16:21 +0530

    init

---
+++ 271Test/lambda.js
@@ -0,0 +1,16 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = function (event, context, callback) {
+
+    ddb.get({
+        TableName: '271Table',
+        Key: { 'name': 'kevin' }
+    }).promise().then(function (data) {
+        //your logic goes here
+    }).catch(function (err) {
+        //handle error
+    });
+
+    callback(null, { "message": "Successfully executed" });
+}
