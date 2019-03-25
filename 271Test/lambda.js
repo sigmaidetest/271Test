a06645669476cc2f5390805976c8c15a1d88fe88
sigmaidetest <cehib@balanc3r.com>
2019-03-25 10:18:20 +0530

    Commented out DB code

--- 271Test/lambda.js
+++ 271Test/lambda.js
@@ -3,14 +3,14 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
 
-    ddb.get({
-        TableName: '271Table',
-        Key: { 'name': 'kevin' }
-    }).promise().then(function (data) {
-        //your logic goes here
-    }).catch(function (err) {
-        //handle error
-    });
+    // ddb.get({
+    //     TableName: '271Table',
+    //     Key: { 'name': 'kevin' }
+    // }).promise().then(function (data) {
+    //     //your logic goes here
+    // }).catch(function (err) {
+    //     //handle error
+    // });
 
     callback(null, { "message": "Successfully executed" });
 }
